818eda5d8ea4f1a39347add8bad47efe5c869417
Dobronezsky <dobronezsky@ya.ru>
2024-02-21 22:00:35 +0700

    style: improved codestyle by actions check

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.ConstraintViolation;
