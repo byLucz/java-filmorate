2c8c91fbefe855f83fe9f356805a7ce1a87712bd
Dobronezsky <dobronezsky@ya.ru>
2024-03-17 22:28:05 +0700

    feat: simplified rest controllers & added endpoints for likes/friends

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,60 +1,59 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
-@Slf4j
 public class UserController {
-    private Map<Integer, User> users = new HashMap<>();
-    private int userId = 1;
+    private final InMemoryUserStorage userStorage;
+
+    private final UserService userService;
+    @Autowired
+    public UserController(InMemoryUserStorage userStorage, UserService userService) {
+        this.userStorage = userStorage;
+        this.userService = userService;
+    }
 
     @GetMapping
     public List<User> allUsers() {
-        log.info("Выведен список всех пользователей");
-        return new ArrayList<>(users.values());
+        return userStorage.allUsers();
     }
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
-        validateUser(user);
-        user.setId(userId);
-        users.put(user.getId(), user);
-        log.info("Добавлен пользователь " + user.getLogin());
-        userId++;
-        return user;
+        return userStorage.createUsers(user);
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
-        validateUser(user);
-        if (!users.containsKey(user.getId())) {
-            log.warn("Такого пользователя не существует " + user.getName());
-            throw new NotFoundException("Пользователя не найдено");
-        }
-        users.put(user.getId(), user);
-        log.info("Обновлен пользователь " + user.getName());
-        return user;
+        return userStorage.updateUsers(user);
     }
 
-    private void validateUser(User user) {
-        if (user == null) {
-            log.warn("Информации о пользователе не предоставлено");
-            throw new ValidationException("Информации о пользователе не предоставлено");
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            log.info("Пустое имя было заменено на логин");
-            user.setName(user.getLogin());
-        }
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable int id, @PathVariable int friendId) {
+        userService.addFriend(id, friendId);
     }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
+        userService.deleteFriend(id, friendId);
+    }
+
+    @GetMapping("/{id}/friends")
+    public List<User> getFriends(@PathVariable int id) {
+        return userService.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
+        return userService.getCommonFriends(id, otherId);
+    }
+
 }
