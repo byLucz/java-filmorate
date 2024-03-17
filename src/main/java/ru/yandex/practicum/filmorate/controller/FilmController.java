de10e83f13f00e38f415af18e7fec4fac4489b83
Dobronezsky <dobronezsky@ya.ru>
2024-03-17 22:37:05 +0700

    style: improved codestyle by actions check

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class FilmController {
     private final InMemoryFilmStorage filmStorage;
     private final FilmService filmService;
+
     @Autowired
     public FilmController(InMemoryFilmStorage filmStorage, FilmService filmService) {
         this.filmStorage = filmStorage;
