29c33e45c4f4c7cf2bc6bb0457c000edb364b43a
dobronezsky <dobronezsky@ya.ru>
2024-03-19 01:39:57 +0700

    feat: added simple getter for users

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserStorage {
     List<User> allUsers();
@@ -12,4 +13,5 @@ public interface UserStorage {
     User updateUsers(User user);
 
     void validateUser(User user);
+    Map<Integer, User> getUsers();
 }
