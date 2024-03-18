d996667e9ab9532758feb5f1105feb36e8ec3051
dobronezsky <dobronezsky@ya.ru>
2024-03-19 01:39:22 +0700

    feat: added log messages for exceptions

--- src/main/java/ru/yandex/practicum/filmorate/controller/ExHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ExHandler.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,11 +11,13 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import java.util.Map;
 
 @RestControllerAdvice("ru.yandex.practicum.filmorate.controller")
+@Slf4j
 
 public class ExHandler {
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidationException(final RuntimeException e) {
+        log.debug("Получен статус 400 Validation Exception {}", e.getMessage(), e);
         return Map.of("ERROR", "Ошибка валидации данных", "ErrorMessage", e.getMessage()
         );
     }
@@ -22,6 +25,7 @@ public class ExHandler {
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFoundException(final RuntimeException e) {
+        log.debug("Получен статус 404 Not Found {}", e.getMessage(), e);
         return Map.of(
                 "ERROR", "Искомый объект не найден", "ErrorMessage", e.getMessage()
         );
@@ -30,6 +34,7 @@ public class ExHandler {
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleOtherException(final RuntimeException e) {
+        log.debug("Получен статус 500 Internal Server Exception {}", e.getMessage(), e);
         return Map.of(
                 "ERROR", "Возникло исключение", "ErrorMessage", e.getMessage()
         );
