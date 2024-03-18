6a0fb6245981fdfb7fefa426696dfb2538da8e1c
dobronezsky <dobronezsky@ya.ru>
2024-03-19 01:45:28 +0700

    fix: rebuild getting of user ids

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -16,61 +16,56 @@ import java.util.Set;
 public class UserService {
     private final UserStorage userStorage;
 
-    @Autowired
     public UserService(UserStorage userStorage) {
         this.userStorage = userStorage;
     }
 
     public void addFriend(int userId, int friendId) {
-        User user = userStorage.allUsers().get(--userId);
-        User friend = userStorage.allUsers().get(--friendId);
+        User user = userStorage.getUsers().get(userId);
+        User friend = userStorage.getUsers().get(friendId);
         Set<Integer> userFriends = user.getFriends();
         Set<Integer> friendFriends = friend.getFriends();
         if (userFriends == null)
             userFriends = new HashSet<>();
-        userFriends.add(++friendId);
+        userFriends.add(friendId);
 
         if (friendFriends == null)
             friendFriends = new HashSet<>();
-        friendFriends.add(++userId);
+        friendFriends.add(userId);
 
-        //user.setFriends(userFriends);
-        //friend.setFriends(friendFriends);
         log.info("Пользователь {} успешно добавлен в друзья пользователю {}",friend.getName(),user.getName());
     }
 
     public void deleteFriend(int userId, int friendId) {
-        User user = userStorage.allUsers().get(--userId);
-        User friend = userStorage.allUsers().get(--friendId);
+        User user = userStorage.getUsers().get(userId);
+        User friend = userStorage.getUsers().get(friendId);
         Set<Integer> userFriends = user.getFriends();
         Set<Integer> friendFriends = friend.getFriends();
-        userFriends.remove(++friendId);
-        friendFriends.remove(++userId);
+        userFriends.remove(friendId);
+        friendFriends.remove(userId);
 
-        //user.setFriends(userFriends);
-        //friend.setFriends(friendFriends);
         log.info("Пользователь {} успешно удален из друзей пользователя {}",friend.getName(),user.getName());
     }
 
     public List<User> getFriends(int userId) {
         List<User> friends = new ArrayList<>();
-        User user = userStorage.allUsers().get(--userId);
+        User user = userStorage.getUsers().get(userId);
         for (Integer spottedId : user.getFriends())
-            friends.add(userStorage.allUsers().get(--spottedId));
+            friends.add(userStorage.getUsers().get(spottedId));
         log.info("Выведен список друзей пользователя {}",user.getName());
         return friends;
     }
 
     public List<User> getCommonFriends(int userId, int friendId) {
         List<User> commonFriends = new ArrayList<>();
-        User user = userStorage.allUsers().get(--userId);
-        User friend = userStorage.allUsers().get(--friendId);
+        User user = userStorage.getUsers().get(userId);
+        User friend = userStorage.getUsers().get(friendId);
         Set<Integer> friendList = new HashSet<>(user.getFriends());
 
         friendList.retainAll(friend.getFriends());
 
         for (Integer spottedId : friendList)
-            commonFriends.add(userStorage.allUsers().get(--spottedId));
+            commonFriends.add(userStorage.getUsers().get(spottedId));
         log.info("Выведен список общих друзей для пользователей {} и {}",friend.getName(),user.getName());
         return commonFriends;
     }
