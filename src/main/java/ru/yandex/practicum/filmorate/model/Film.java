59db4f18da36fe5213d8ace8e5053d1a78ff416d
Dobronezsky <dobronezsky@ya.ru>
2024-03-27 02:31:23 +0700

    feat: added method for genre storiging

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,14 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Data
@@ -23,14 +22,22 @@ public class Film {
     private LocalDate releaseDate;
     @Positive
     private int duration;
-    private Integer ratingID;
 
-    public Film(Integer id, String name, String description, LocalDate releaseDate, int duration, Integer ratingID) {
+    private MPA mpa;
+
+    private LinkedHashSet<Genre> genres = new LinkedHashSet<>();
+
+    public Film(Integer id, String name, String description, LocalDate releaseDate, int duration, MPA mpa) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.releaseDate = releaseDate;
         this.duration = duration;
-        this.ratingID = ratingID;
+        this.mpa = mpa;
+    }
+
+    public void addGenre(Genre genre)
+    {
+        genres.add(genre);
     }
 }
