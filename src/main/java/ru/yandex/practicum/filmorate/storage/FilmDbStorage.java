586804977f6ded70cd1c88b2f73d35611b62dc1f
Dobronezsky <dobronezsky@ya.ru>
2024-03-28 17:46:34 +0700

    feat: transported db operations from service & structure improving

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -1,12 +1,12 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
@@ -16,10 +16,8 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Component
-@Qualifier("filmDbStorage")
 @Slf4j
 public class FilmDbStorage implements FilmStorage {
     private JdbcTemplate jdbcTemplate;
@@ -45,8 +43,12 @@ public class FilmDbStorage implements FilmStorage {
 
         Integer genreId = rs.getObject("genre_id", Integer.class);
         if (genreId != null) {
-            Genre genre = new Genre(genreId,rs.getString("genre_name"));
-            film.addGenre(genre);
+            List<Genre> genres = getFilmGenres(film.getId());
+            for (Genre g : genres)
+            {
+                film.addGenre(g);
+            }
+
         }
 
         Integer likes = rs.getObject("likes", Integer.class);
@@ -71,6 +73,7 @@ public class FilmDbStorage implements FilmStorage {
                 "LEFT JOIN LIKES l ON l.FILM_ID = f.ID";
         return jdbcTemplate.query(sql, filmRowMapper);
     }
+
     @Override
     public Film createFilm(Film film) {
         validateFilm(film);
@@ -89,7 +92,7 @@ public class FilmDbStorage implements FilmStorage {
         int genFilmId = (int) keyHolder.getKey();
         film.setId(genFilmId);
         updateGenre(genFilmId,film.getGenres());
-        return film;
+        return getFilmById(genFilmId);
     }
 
     @Override
@@ -98,20 +101,80 @@ public class FilmDbStorage implements FilmStorage {
         String sql = "UPDATE PUBLIC.FILM SET name = ?, description = ?, release_date = ?, duration = ?, rating_id = ? WHERE id = ?";
         jdbcTemplate.update(sql, film.getName(), film.getDescription(), film.getReleaseDate(), film.getDuration(), film.getMpa().getId(), film.getId());
         updateGenre(film.getId(),film.getGenres());
-        return film;
+        return getFilmById(film.getId());
     }
 
-    public Map<Integer, Film> getFilms() {
-        return allFilms().stream().collect(Collectors.toMap(Film::getId, film -> film));
+    @Override
+    public Film getFilmById(int id) {
+        String sql = "SELECT f.ID as film_id, f.NAME as name, f.DESCRIPTION as description, " +
+                "f.RELEASE_DATE as release_date, f.DURATION as duration, " + "r.ID as rating_id,"+
+                "r.RATING_CODE as rating_code, r.DESCRIPTION as rating_description, " + "g.ID as genre_id," +
+                "g.NAME as genre_name, l.USER_ID AS likes " +
+                "FROM FILM as f " +
+                "LEFT JOIN RATING as r ON r.id = f.RATING_ID " +
+                "LEFT JOIN FILMGENRE fg ON fg.FILM_ID = f.ID " +
+                "LEFT JOIN GENRE g ON g.ID = fg.GENRE_ID " +
+                "LEFT JOIN LIKES l ON l.FILM_ID = f.ID " +
+                "WHERE f.ID = ? ";
+
+        List<Film> films = jdbcTemplate.query(sql, filmRowMapper, id);
+        if (films .isEmpty())
+            throw new NotFoundException("Фильм с ID " + id + " не найден");
+        return films.get(0);
+    }
+
+    @Override
+    public void addLikeToDB(Integer userId, Integer filmId) {
+        String sql = "INSERT INTO LIKES(user_id, film_id) VALUES (?,?)";
+        jdbcTemplate.update(sql, userId, filmId);
+    }
+
+    @Override
+    public void deleteLikeFromDB(Integer userId, Integer filmId) {
+        String sql = "DELETE FROM LIKES l WHERE l.USER_ID=" + userId + " and l.FILM_ID =" + filmId;
+        jdbcTemplate.execute(sql);
+    }
+
+    @Override
+    public List<Film> findPopularFilms(int count) {
+        String sql = "SELECT f.*, " +
+                "(SELECT COUNT(*) FROM likes WHERE film_id = f.id) AS likes_count " +
+                "FROM film AS f " +
+                "ORDER BY likes_count DESC " +
+                "LIMIT ?";
+        return jdbcTemplate.query(sql, new Object[]{count}, (rs, rowNum) -> {
+            Film film = new Film();
+            film.setId(rs.getInt("id"));
+            film.setName(rs.getString("name"));
+            return film;
+        });
     }
 
     private void updateGenre(int id, Set<Genre> gSet) {
-        jdbcTemplate.update("DELETE FROM FILMGENRE WHERE FILM_ID = ?", id);
-        if (gSet != null && !gSet.isEmpty())
+        if (gSet != null && !gSet.isEmpty()) {
+            List<Object[]> batchArgs = new ArrayList<>();
             for (Genre g : gSet) {
-                jdbcTemplate.update("INSERT INTO FILMGENRE(FILM_ID,GENRE_ID) VALUES(?,?)", id, g.getId());
+                batchArgs.add(new Object[]{id, g.getId()});
             }
+            jdbcTemplate.batchUpdate("INSERT INTO FILMGENRE (FILM_ID, GENRE_ID) VALUES (?, ?)", batchArgs);
+        }
     }
+
+    private List<Genre> getFilmGenres(int filmId)
+    {
+        String sql = "SELECT g.id, g.name " +
+                "FROM (SELECT * " +
+                "FROM FILMGENRE f " +
+                "WHERE film_id = ?) AS f " +
+                "JOIN GENRE AS g ON f.genre_id = g.id";
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            return new Genre(id, name);
+        }, filmId);
+    }
+
     private void validateFilm(Film film) {
         if (film == null) {
             log.warn("Информации о фильме не предоставлено");
@@ -147,4 +210,5 @@ public class FilmDbStorage implements FilmStorage {
         int count = jdbcTemplate.queryForObject(sql, Integer.class, mpaId);
         return count > 0;
     }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -13,16 +12,14 @@ import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 @Component
-@Qualifier("userDbStorage")
 @Slf4j
 public class UserDbStorage implements UserStorage{
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDbStorage(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -38,12 +35,17 @@ public class UserDbStorage implements UserStorage{
 
         Integer friendId = rs.getObject("friend_id", Integer.class);
         if (friendId != null) {
-            if (user.getFriends() == null)
-                user.setFriends(new HashSet<>());
-            user.getFriends().add(friendId);
+            Set<Integer> friends = user.getFriends();
+
+            if (friends == null) {
+                friends = new LinkedHashSet<>();
+                user.setFriends(friends);
+            }
+            friends.add(friendId);
         }
         return user;
     };
+
     @Override
     public List<User> allUsers() {
         String sql = "SELECT u.ID AS id, u.name AS name, u.LOGIN AS login, " +
@@ -67,8 +69,8 @@ public class UserDbStorage implements UserStorage{
         }, keyHolder);
 
         int genUserId = (int) keyHolder.getKey();
-        user.setId(genUserId);
-        return user;
+
+        return getUserById(genUserId);
     }
 
     @Override
@@ -81,12 +83,74 @@ public class UserDbStorage implements UserStorage{
         }
         String sql = "UPDATE PUBLIC.USERS SET email = ?, login = ?, name = ?, birthday = ? WHERE id = ?";
         jdbcTemplate.update(sql, user.getEmail(), user.getLogin(), user.getName(), user.getBirthday(), user.getId());
-        return user;
+        return getUserById(user.getId());
     }
 
     @Override
-    public Map<Integer, User> getUsers() {
-        return allUsers().stream().collect(Collectors.toMap(User::getId, user -> user));
+    public User getUserById(int id) {
+        String sql = "SELECT u.ID AS id, u.name AS name, u.LOGIN AS login, " +
+                "u.EMAIL AS mail, u.BIRTHDAY AS birthday, f.FRIEND_ID AS friend_id " +
+                "FROM USERS u LEFT JOIN FRIENDSHIP f ON f.FRIEND_ID = u.ID " +
+                "WHERE u.ID = ?";
+
+        List<User> users = jdbcTemplate.query(sql, userRowMapper, id);
+        if (users.isEmpty())
+            throw new NotFoundException("Пользователь с ID " + id + " не найден");
+        return users.get(0);
+    }
+
+    @Override
+    public void addFriend(int userId, int friendId) {
+        deleteFriend(userId, friendId);
+        if (!isUserExists(userId) || !isUserExists(friendId)) {
+            throw new NotFoundException("Такого пользователя или друга не существует");
+        }
+        if (areFriends(userId, friendId)) {
+            throw new ValidationException("Пользователи уже являются друзьями");
+        }
+        String sql = "INSERT INTO FRIENDSHIP (user_id, friend_id) VALUES (?, ?)";
+        jdbcTemplate.update(sql, userId, friendId);
+        updateFriendshipStatus(userId,friendId);
+    }
+
+    @Override
+    public void deleteFriend(int userId, int friendId) {
+        if (!isUserExists(userId) || !isUserExists(friendId)) {
+            throw new NotFoundException("Такого пользователя или друга не существует");
+        }
+        String deleteSql = "DELETE FROM FRIENDSHIP WHERE USER_ID=" + userId + " and FRIEND_ID=" + friendId;
+        jdbcTemplate.execute(deleteSql);
+        updateFriendshipStatus(userId,friendId);
+    }
+
+    @Override
+    public List<User> getFriendsList(int userId) {
+        String sql = "SELECT f.FRIEND_ID FROM FRIENDSHIP f WHERE f.USER_ID = ?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            int friendId = rs.getInt("FRIEND_ID");
+            return getUserById(friendId);
+        }, userId);
+    }
+
+    @Override
+    public List<User> getCommonFriendsFromDB(int userId, int friendId)
+    {
+        String sql = "SELECT * FROM users " +
+                "WHERE id IN (SELECT friend_id FROM friendship " +
+                "WHERE user_id = ? INTERSECT " +
+                "SELECT friend_id FROM friendship WHERE user_id = ?)";
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            int commonFriendsId = rs.getInt("ID");
+            return getUserById(commonFriendsId);
+        }, userId, friendId);
+    }
+
+    @Override
+    public boolean isUserExists(int userId) {
+        String sql = "SELECT COUNT(*) FROM USERS WHERE ID = ?";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class, userId);
+        return count > 0;
     }
 
     private void validateUser(User user) {
@@ -100,5 +164,22 @@ public class UserDbStorage implements UserStorage{
         }
     }
 
+    public void updateFriendshipStatus(int userId, int friendId) {
+        String sql = "SELECT COUNT(*) AS count " +
+                "FROM FRIENDSHIP " +
+                "WHERE (USER_ID = ? AND FRIEND_ID = ?) OR (USER_ID = ? AND FRIEND_ID = ?)";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class, userId, friendId, friendId, userId);
+
+        String status = (count == 2) ? "confirmed" : "unconfirmed";
+        sql = "UPDATE FRIENDSHIP SET STATUS = ? " +
+                "WHERE (USER_ID = ? AND FRIEND_ID = ?) OR (USER_ID = ? AND FRIEND_ID = ?)";
+        jdbcTemplate.update(sql, status, userId, friendId, friendId, userId);
+    }
+
+    private boolean areFriends(int userId, int friendId) {
+        String sql = "SELECT COUNT(*) FROM FRIENDSHIP WHERE (user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class, userId, friendId, friendId, userId);
+        return count > 0;
+    }
 
 }
