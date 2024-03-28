4b4ef3d96975016764fc198bdc576eb1664000f9
Dobronezsky <dobronezsky@ya.ru>
2024-03-28 17:47:57 +0700

    feat: improving biz logics & better checking data

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,53 +1,45 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class FilmService {
-    private final JdbcTemplate jdbcTemplate;
     private final FilmStorage filmStorage;
 
-    public FilmService(JdbcTemplate jdbcTemplate, @Qualifier("filmDbStorage") FilmStorage filmStorage) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.filmStorage = filmStorage;
-    }
+    private final UserStorage userStorage;
 
     public Film addLike(Integer filmId, Integer userId) {
-        Film film = filmStorage.getFilms().get(filmId);
-        String sql = "INSERT INTO LIKES(user_id, film_id) VALUES (?,?)";
-        jdbcTemplate.update(sql, userId, filmId);
-        log.info("Пользователь c id {} успешно поставил лайк фильму {}",userId,film.getName());
+        Film film = filmStorage.getFilmById(filmId);
+        User user = userStorage.getUserById(userId);
+        filmStorage.addLikeToDB(userId, filmId);
+        log.info("Пользователь {} успешно поставил лайк фильму {}",user.getName(),film.getName());
         return filmStorage.updateFilm(film);
     }
 
     public Film deleteLike(Integer filmId, Integer userId) {
-        Film film = filmStorage.getFilms().get(filmId);
+        Film film = filmStorage.getFilmById(filmId);
+        User user = userStorage.getUserById(userId);
         if (!film.getLikeCounter().contains(userId))
             throw new NotFoundException("Этот фильм не получал оценок от этого пользователя");
-        String sql = "DELETE FROM LIKES l WHERE l.USER_ID=" + userId + " and l.FILM_ID =" + filmId;
-        jdbcTemplate.execute(sql);
-        log.info("Пользователь c id {} успешно удалил лайк фильму {}",userId,film.getName());
+        filmStorage.deleteLikeFromDB(userId, filmId);
+        log.info("Пользователь {} успешно удалил лайк фильму {}",user.getName(),film.getName());
         return filmStorage.updateFilm(film);
     }
 
     public List<Film> getPopular(int count) {
         log.info("Вывод {} самых популярных фильмов", count);
-        return filmStorage.allFilms().stream()
-                .sorted(Comparator.comparingInt(film -> -1 * film.getLikeCounter().size()))
-                .limit(count)
-                .collect(Collectors.toList());
+        return filmStorage.findPopularFilms(count);
     }
-
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,107 +1,48 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UserService {
-    private final JdbcTemplate jdbcTemplate;
     private final UserStorage userStorage;
 
-    public UserService(JdbcTemplate jdbcTemplate, @Qualifier("userDbStorage") UserStorage userStorage) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.userStorage = userStorage;
-    }
-
     public void addFriend(int userId, int friendId) {
-        if (!isUserExists(userId) || !isUserExists(friendId)) {
-            throw new NotFoundException("Такого пользователя или друга не существует");
-        }
-        if (areFriends(userId, friendId)) {
-            throw new ValidationException("Пользователи уже являются друзьями");
-        }
-        updateFriendshipStatus(userId,friendId);
-        String sql = "INSERT INTO FRIENDSHIP (user_id, friend_id) VALUES (?, ?)";
-        jdbcTemplate.update(sql, userId, friendId);
-        log.info("Пользователь с id={} успешно добавлен в друзья к пользователю с id={}", friendId, userId);
+        User user = userStorage.getUserById(userId);
+        User friend = userStorage.getUserById(friendId);
+        userStorage.addFriend(userId,friendId);
+        log.info("Пользователь {} успешно добавлен в друзья к пользователю {}", friend.getName(), user.getName());
     }
 
 
     public void deleteFriend(int userId, int friendId) {
-        if (!isUserExists(userId) || !isUserExists(friendId)) {
-            throw new NotFoundException("Такого пользователя или друга не существует");
-        }
-        String deleteSql = "DELETE FROM FRIENDSHIP WHERE USER_ID=" + userId + " and FRIEND_ID=" + friendId;
-        jdbcTemplate.execute(deleteSql);
-        updateFriendshipStatus(userId,friendId);
-
-        log.info("Пользователь {} успешно удален из друзей пользователя {}",friendId,userId);
+        User user = userStorage.getUserById(userId);
+        User friend = userStorage.getUserById(friendId);
+        userStorage.deleteFriend(userId,friendId);
+        log.info("Пользователь {} успешно удален из друзей пользователя {}", friend.getName(), user.getName());
     }
 
-    public List<User> getFriendsList(int userId){
-        String sql = "SELECT f.FRIEND_ID FROM FRIENDSHIP f WHERE f.USER_ID = ?";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            int friendId = rs.getInt("FRIEND_ID");
-            return userStorage.getUsers().get(friendId);
-        }, userId);
+    public List<User> getFriendsList(int userId) {
+        User user = userStorage.getUserById(userId);
+        return userStorage.getFriendsList(userId);
     }
 
     public List<User> getCommonFriends(int userId, int friendId) {
-        if (!isUserExists(userId) || !isUserExists(friendId)) {
+        if (!userStorage.isUserExists(userId) || !userStorage.isUserExists(friendId)) {
             throw new NotFoundException("Такого пользователя или друга не существует");
         }
-
-        List<User> commonFriends = new ArrayList<>();
-        User user = userStorage.getUsers().get(userId);
-        User friend = userStorage.getUsers().get(friendId);
-
-        Set<Integer> commonFriendIds = new HashSet<>(user.getFriends());
-        commonFriendIds.retainAll(friend.getFriends());
-
-        Set<Integer> addedFriendIds = new HashSet<>();
-
-        for (Integer commonFriendId : commonFriendIds) {
-            if (!addedFriendIds.contains(commonFriendId)) {
-                commonFriends.add(userStorage.getUsers().get(commonFriendId));
-                addedFriendIds.add(commonFriendId);
-            }
-        }
-
+        User user = userStorage.getUserById(userId);
+        User friend = userStorage.getUserById(friendId);
         log.info("Выведен список общих друзей для пользователей {} и {}", friend.getName(), user.getName());
-        return commonFriends;
-    }
-    public void updateFriendshipStatus(int userId, int friendId) {
-        String sql = "SELECT COUNT(*) AS count " +
-                "FROM FRIENDSHIP " +
-                "WHERE (USER_ID = ? AND FRIEND_ID = ?) OR (USER_ID = ? AND FRIEND_ID = ?)";
-        int count = jdbcTemplate.queryForObject(sql, Integer.class, userId, friendId, friendId, userId);
-
-        String status = (count == 2) ? "confirmed" : "unconfirmed";
-        sql = "UPDATE FRIENDSHIP SET STATUS = ? " +
-                "WHERE (USER_ID = ? AND FRIEND_ID = ?) OR (USER_ID = ? AND FRIEND_ID = ?)";
-        jdbcTemplate.update(sql, status, userId, friendId, friendId, userId);
-    }
-    public boolean isUserExists(int userId) {
-        String sql = "SELECT COUNT(*) FROM USERS WHERE ID = ?";
-        int count = jdbcTemplate.queryForObject(sql, Integer.class, userId);
-        return count > 0;
+        return userStorage.getCommonFriendsFromDB(userId,friendId);
     }
 
-    private boolean areFriends(int userId, int friendId) {
-        String sql = "SELECT COUNT(*) FROM FRIENDSHIP WHERE (user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)";
-        int count = jdbcTemplate.queryForObject(sql, Integer.class, userId, friendId, friendId, userId);
-        return count > 0;
-    }
 }
